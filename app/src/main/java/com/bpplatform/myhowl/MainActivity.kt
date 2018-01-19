85058554039b2850284f1fc4ca64f8e4dbcc1cff
유명식 <myeongsic@MacBook-Pro.local>
2018-01-19 17:18:00 +0900

    init

---
+++ app/src/main/java/com/bpplatform/myhowl/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.bpplatform.myhowl
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+}
